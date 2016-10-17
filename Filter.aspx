14ef9e199db18e2d0e984c1b208cb7191aaa0b7f
AnnikaWespe <annika.westphaeling@cgi.com>
2016-10-17 12:16:49 +0200

    Add files via upload

--- Filter.aspx
+++ Filter.aspx
@@ -2,11 +2,39 @@
 <html>
 <head>
 	<title></title>
+	<link href="/sites/GWTZ/scripts/bootstrap.min.css" rel="stylesheet" media="screen">
 	<script src="/sites/GWTZ/scripts/jquery-1.12.4.min.js" type="text/javascript"></script>
+	<script src="/sites/GWTZ/scripts/bootstrap.min.js"></script>
 	<script src="/sites/GWTZ/scripts/sharepointplus-3.13.min.js" type="text/javascript"></script>
 	<script src="/sites/GWTZ/scripts/Filterfunktion/Filter.js" type="text/javascript"></script>
 </head>
 <body>
+<table class="table" id="table">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
 
 </body>
 </html>
--- Filter.js
+++ Filter.js
@@ -2,22 +2,28 @@ var columns = {
     bmwSach: "BMW_x002d_Sach_x002d_Nr_x002e__x0020__x0028_BMW_x0020_part_x0020_number_x0029_"
 };
 $(document).ready(function() {
+    console.log("speak, I'm listening");
     $SP().list("Prüfberichte").info(function(fields) {
         var nameDisplaynameMap = {};
+        var displayedColumns = [];
         for (var i = 0; i < fields.length; i++) {
             var key = fields[i]["Name"];
             var value = fields[i]["DisplayName"];
             nameDisplaynameMap[key] = value;
         };
-        console.log("nameDisplaynameMap: " + nameDisplaynameMap.BMW_x002d_Sach_x002d_Nr_x002e__x0020__x0028_BMW_x0020_part_x0020_number_x0029_);
-        //console.log(fields[i]["Name"] + ": " + fields[i]["DisplayName"]);
+
+        $SP().list("Prüfberichte").view("Alle Dokumente", function(data, viewID) {
+            for (var i = 0; i < data.fields.length; i++) {
+                var displayedColumn = data.fields[i];
+                displayedColumns.push(nameDisplaynameMap[displayedColumn])
+            }
+
+        });
+
+        //$("#table").hide();
+
     });
 
-    $SP().list("Prüfberichte").view("Alle Dokumente", function(data, viewID) {
-        for (var i = 0; i < data.fields.length; i++)
-        /*console.log("Column " + i + ": " + data.fields[i]["DisplayName"]);
-                console.log("And the GUI for this view is :" + viewID);*/
-            console.log(data.fields[0]);
-    });
+
 
 });

74a41b96c52b3c79eca542f3497b430da0e8a7c7
AnnikaWespe <annika.westphaeling@cgi.com>
2016-10-17 18:50:45 +0200

    Add files via upload

--- Filter.js
+++ Filter.js
@@ -2,28 +2,47 @@ var columns = {
     bmwSach: "BMW_x002d_Sach_x002d_Nr_x002e__x0020__x0028_BMW_x0020_part_x0020_number_x0029_"
 };
 $(document).ready(function() {
-    console.log("speak, I'm listening");
     $SP().list("Prüfberichte").info(function(fields) {
         var nameDisplaynameMap = {};
         var displayedColumns = [];
+        var valuesForDropDown = {};
         for (var i = 0; i < fields.length; i++) {
             var key = fields[i]["Name"];
             var value = fields[i]["DisplayName"];
             nameDisplaynameMap[key] = value;
+            valuesForDropDown[key] = [];
         };
 
-        $SP().list("Prüfberichte").view("Alle Dokumente", function(data, viewID) {
-            for (var i = 0; i < data.fields.length; i++) {
-                var displayedColumn = data.fields[i];
-                displayedColumns.push(nameDisplaynameMap[displayedColumn])
-            }
+        $SP().list("Prüfberichte").view("Alle Dokumente", function(dataView, viewID) {
+            for (var i = 0; i < dataView.fields.length; i++) {
+                var displayedColumn = dataView.fields[i];
+                console.log(displayedColumn);
+                displayedColumns.push(nameDisplaynameMap[displayedColumn]);
+            };
+            var lengthDisplayedColumns = displayedColumns.length;
+            for (var i = 0; i < lengthDisplayedColumns; i++) {
+                $("#table").find("thead").find("tr").append("<th>" + displayedColumns[i] + "</th>");
+            };
+            $SP().list("Prüfberichte").get(function(data) {
+                for (var i = 0; i < data.length; i++) {
+                    var tablebody = $("#table").find("tbody");
+                    tablebody.append("<tr>");
+                    for (var j = 0; j < dataView.fields.length; j++) {
+                        var currentColumnName = dataView.fields[j];
+                        var currentEntry = data[i].getAttribute(currentColumnName);
+                        if (currentColumnName == "LinkFilename") {
+                            tablebody.append('<td><a href="/sites/GWTZ/Prfberichte/' + currentEntry + '">' + currentEntry + "</a></td> ");
+                        } else {
+                            tablebody.append("<td>" + currentEntry + "</td>");
+                        };
+                        if (valuesForDropDown[currentColumnName].indexOf(currentEntry) == -1) {
+                            valuesForDropDown[currentColumnName].push(currentEntry)
+                        };
+                    };
+                    tablebody.append("</tr>");
+                };
 
+            });
         });
-
-        //$("#table").hide();
-
     });
-
-
-
 });

46db7ab7278cf61000962ab994a347c39687340b
AnnikaWespe <annika.westphaeling@cgi.com>
2016-10-31 15:24:06 +0100

    moment.js added

--- Filter.aspx
+++ Filter.aspx
@@ -5,6 +5,7 @@
 		
 		<script src="/sites/GWTZ/scripts/jquery-1.12.4.min.js" type="text/javascript"></script>
 		<script src="/sites/GWTZ/scripts/chosen.jquery.js" type="text/javascript"></script>
+		<script src="/sites/GWTZ/scripts/moment.js" type="text/javascript"></script>
 		<script src="/sites/GWTZ/scripts/bootstrap.min.js"></script>
 		<script src="/sites/GWTZ/scripts/sharepointplus-3.13.min.js" type="text/javascript"></script>
 		<script src="/sites/GWTZ/scripts/Filterfunktion/Filter.js" type="text/javascript"></script>
@@ -18,6 +19,7 @@
 		<button class="btn btn-primary active" id="filter">Filter anwenden/Apply Filter</button>
 		<button class="btn btn-primary active" id="filterReset">Filter reset</button>
 		<button class="btn btn-primary disabled_special" disabled id="numberOfResults">Anzahl Ergebnisse</button>
+		<button class="btn btn-primary active export" id="export">Ergebnisse als pdf exportieren / export results</button>
 		<table class="table table-striped" id="table">
 			<thead>
 			<tr></tr>
--- Filter.js
+++ Filter.js
@@ -1,3 +1,17 @@
+//Strings
+var productionDateInputField = "#inputVonBisProd\\.\\-Datum\\ Fzg\\.\\ \\(Production\\ date\\)";
+var mileageInputField = "#inputVonBiskm\\-Stand\\ \\(Mileage\\)";
+var berichtsNrInputField = "#inputVonBisBerichts-Nr\\.";
+var placeholder = {};
+placeholder["Berichts-Nr."] = "yyyy_xxxx-yyyy_xxxx";
+placeholder["km-Stand (Mileage)"] = "Eingabe ohne (.) und (,) / Enter without (.) or (,)";
+placeholder["Prod.-Datum Fzg. (Production date)"] = "dd.mm.yyyy-dd.mm.yyyy";
+var noPropertiesAlert = "Bitte wählen Sie mindestens ein Filterkriterium aus!\n\nPlease select at least one property to filter by!";
+var BerichtsNrInputAlert = "Feld Berichts-Nr.:\n\nBitte geben Sie den Bereich für die Berichts-Nr. ohne Leerzeichen ein.\nPlease enter the range for the report number w/o blanks.\n\nExamples:\n\n2016_0001-2017_0001\n\n2017_1234-2017_1234";
+var dateAlert = "Feld Produktionsdatum / production date:\n\nBitte geben Sie Start- und Enddatum im Format tt.mm.jjjj ein.\nPlease enter the start and end date in the format dd.mm.yyyy\n\nExample:\n\n01.01.2016-01.01.2017";
+var mileageAlert = "Feld km-Stand / mileage: \n\nBitte geben Sie eine ganze Zahl ohne (.) oder (,) ein\nPlease enter a whole number (no decimals) without (.) or (,)";
+
+
 $(document).ready(function() {
     $SP().list("Prüfberichte").info(function(fields) {
         var nameDisplaynameMap = {};
@@ -23,18 +37,15 @@ $(document).ready(function() {
                 var currentColumn = dataView.fields[i];
                 var currentDisplayName = nameDisplaynameMap[currentColumn];
                 var columnsEnterVonBis = ["Berichts-Nr.", "km-Stand (Mileage)", "Prod.-Datum Fzg. (Production date)"];
-                var placeholder = {};
-                placeholder["Berichts-Nr."] = "yyyy_xxxx-yyyy_xxxx";
-                placeholder["km-Stand (Mileage)"] = "Eingabe ohne (.) und (,) / Enter without (.) or (,)";
-                placeholder["Prod.-Datum Fzg. (Production date)"] = "dd.mm.yyyy-dd.mm.yyyy";
                 displayedColumns.push(currentDisplayName);
                 displayNameColumnNumberMap[currentDisplayName] = i;
+                //create input fields where necessary
                 if (columnsEnterVonBis.indexOf(currentDisplayName) !== -1) {
                     $("#selectBox").append('<div class="col-md-3" id="column' + i + '"><label for="inputVonBis' +
                         displayedColumns[i] + '">' + displayedColumns[i] + '</label><input class="inputVonBis" id="inputVonBis' +
                         displayedColumns[i] + '" placeholder="' + placeholder[displayedColumns[i]] + '" name="' +
                         displayedColumns[i] + '" type = "text"></div>');
-                } else {
+                } else { //create select fields where necessary
                     $("#selectBox").append('<div class="col-md-3" id="column' + i + '"><label for="select' + displayedColumns[i] +
                         '">' + displayedColumns[i] + '</label><select id="select' + displayedColumns[i] +
                         '" data-placeholder="auswählen / select" name="' + displayedColumns[i] +
@@ -69,10 +80,13 @@ $(document).ready(function() {
                                 console.log(i + valuesBerichtsNr[i]);
                             }
                         } else {
-                            if (j == 10 && currentEntry) {
+                            if (j == 2 && currentEntry) {
+                                var date = moment(currentEntry);
+                                console.log(date);
+                            } else if (j == 10 && currentEntry) {
                                 var helperArray = currentEntry.split(".");
                                 currentEntry = helperArray[0];
-                            };
+                            }
                             stringToAppend += '<td id="' + currentPosition + '">' + currentEntry + "</td>";
                         };
                         if (valuesForDropDown[currentColumnName].indexOf(currentEntry) == -1) {
@@ -99,38 +113,40 @@ $(document).ready(function() {
                 $("#filter").click(function() {
                     var filterBy = {};
                     var selectedProperties = [];
-                    var numberOfProperties = selectedProperties.length;
-                    if (checkMileage() && checkDatum() && checkBerichtsNr()) {
-                        alert("all is clear")
-                    }
-                    $("select").each(function() {
-                        var selectedValues = $(this).val();
-                        var propertyName = $(this).attr('name');
-                        var columnNumber = displayNameColumnNumberMap[propertyName];
-                        if (selectedValues !== null) {
-                            filterBy[columnNumber] = selectedValues;
-                            selectedProperties.push(columnNumber);
-                        }
-                    });
-                    if (numberOfProperties == 0) {
-                        alert("Bitte wählen Sie mindestens ein Filterkriterium aus!\n\nPlease select at least one property to filter by!")
-                    } else {
-                        //makes it possible to readjust filter after filter already clicked once
-                        if (filterAlreadyClicked) {
-                            $("[id*='row']").show();
-                            $("#numberOfResults").html("Anzahl Ergebnisse");
-                        }
-                        for (var i = 0; i < numberOfProperties; i++) {
-                            for (var j = 0; j < numberOfRows; j++) {
-                                if (filterBy[selectedProperties[i]].indexOf($("#row" + j + "column" + selectedProperties[i]).html()) == -1) {
-                                    $("#row" + j).hide();
+                    var numberOfProperties;
+                    if (checkBerichtsNr() && checkDatum() && checkMileage()) {
+                        $("select").each(function() {
+                            var selectedValues = $(this).val();
+                            var propertyName = $(this).attr('name');
+                            var columnNumber = displayNameColumnNumberMap[propertyName];
+                            if (selectedValues !== null) {
+                                filterBy[columnNumber] = selectedValues;
+                                selectedProperties.push(columnNumber);
+                            }
+                        });
+                        numberOfProperties = selectedProperties.length;
+                        if (numberOfProperties == 0) {
+                            alert(noPropertiesAlert)
+                        } else {
+                            //makes it possible to readjust filter criteria after filter already clicked once
+                            if (filterAlreadyClicked) {
+                                $("[id*='row']").show();
+                                $("#numberOfResults").html("Anzahl Ergebnisse");
+                            }
+                            //check for each entry if compatible with the selected criteria
+                            for (var i = 0; i < numberOfProperties; i++) {
+                                for (var j = 0; j < numberOfRows; j++) {
+                                    if (filterBy[selectedProperties[i]].indexOf($("#row" + j + "column" + selectedProperties[i]).html()) == -1) {
+                                        $("#row" + j).hide();
+                                    }
                                 }
                             }
-                        }
-                        var numberOfRowsjQuery = $(".tablerow:visible").not('[style *= "display:none"]').length;
-                        $("#numberOfResults").append(": " + numberOfRowsjQuery);
-                        filterAlreadyClicked = 1;
-                    };
+                            //Filter by Berichts-Nr. range
+                            var numberOfRowsjQuery = $(".tablerow:visible").not('[style *= "display:none"]').length;
+                            $("#numberOfResults").append(": " + numberOfRowsjQuery);
+                            filterAlreadyClicked = 1;
+                        };
+                    }
                 });
                 $("#filterReset").click(function() {
                     $("[id*='row']").show();
@@ -140,43 +156,43 @@ $(document).ready(function() {
                     $(".inputVonBis").val('');
                     filterAlreadyClicked = 0;
                 });
-                $("#inputVonBisBerichts-Nr\\.").focusout(checkBerichtsNr);
-                $("#inputVonBisProd\\.\\-Datum\\ Fzg\\.\\ \\(Production\\ date\\)").focusout(checkDatum);
-                $("#inputVonBiskm\\-Stand\\ \\(Mileage\\)").focusout(checkMileage);
+                $(berichtsNrInputField).focusout(checkBerichtsNr);
+                $(productionDateInputField).focusout(checkDatum);
+                $(mileageInputField).focusout(checkMileage);
             });
         });
     });
     var checkBerichtsNr = function() {
-        var inputToBeChecked = $("#inputVonBisBerichts-Nr\\.").val();
+        var inputToBeChecked = $(berichtsNrInputField).val();
         if (inputToBeChecked != '') {
             var testRegExp = /\d\d\d\d_\d\d\d\d-\d\d\d\d_\d\d\d\d/;
             var res = testRegExp.test(inputToBeChecked);
             if (!res) {
-                alert("Bitte geben Sie den Bereich für die Berichts-Nr. ohne Leerzeichen ein." +
-                    "\nPlease enter the range for the report number w/o blanks.\n\nExample:\n\n2016_0001-2017_0001\n\n2017_1234-2017_1234");
-            }
-        }
+                alert(BerichtsNrInputAlert);
+                return false;
+            } else return true;
+        } else return true;
     }
     var checkDatum = function() {
-        var inputToBeChecked = $("#inputVonBisProd\\.\\-Datum\\ Fzg\\.\\ \\(Production\\ date\\)").val();
+        var inputToBeChecked = $(productionDateInputField).val();
         if (inputToBeChecked != '') {
             var testRegExp = /\d\d\.\d\d\.\d\d\d\d\-\d\d\.\d\d\.\d\d\d\d/;
             var res = testRegExp.test(inputToBeChecked);
             if (!res) {
-                alert("Bitte geben Sie Start- und Enddatum im Format tt.mm.jjjj ein." +
-                    "\nPlease enter the start and end date in the format dd.mm.yyyy\n\nExamples:\n\n01.01.2016-01.01.2017");
-            }
-        }
+                alert(dateAlert);
+                return false;
+            } else return true;
+        } else return true;
     }
     var checkMileage = function() {
-        var inputToBeChecked = $("#inputVonBiskm\\-Stand\\ \\(Mileage\\)").val();
+        var inputToBeChecked = $(mileageInputField).val();
         var testRegExp = /^\d+$/;
         var res = testRegExp.test(inputToBeChecked);
         if (inputToBeChecked != '') {
             if (!res) {
-                alert("Bitte geben Sie eine ganze Zahl ohne (.) oder (,) ein\nPlease enter a whole number (no decimals) without (.) or (,)")
+                alert(mileageAlert);
                 return false;
-            }
-        }
+            } else return true;
+        } else return true;
     }
 });
